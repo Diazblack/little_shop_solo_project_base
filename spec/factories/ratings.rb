11e0df7e94dfb6fa28f0d62e687dd0f8f1e8137f
Cesar Jolibois <diazcd85@gmail.com>
2018-11-06 15:08:10 -0700

    user can't not see the link to reviews if that item was already rate

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :rating do
+    user
+    order_item
     sequence :title       { |n| "Title #{n}" }
-    sequence :description { "My text #{n}" }
+    sequence :description { |n| "My text #{n}" }
     rate                  { 3 }
-    users
-    order_items
   end
 end
